a5840e223b7992e1dad45ea500b6657ca2b8be1e
BloodworthInc <201190162+BloodworthInc@users.noreply.github.com>
2025-03-17 23:32:03 -0500

    initial commit

---
+++ next.js
@@ -0,0 +1,8 @@
+/your-project
+├── /pages
+│    ├── index.js         // Your homepage
+│    └── api              // (Optional) API endpoints
+├── /components          // (Optional) Reusable UI components
+├── package.json
+├── next.config.js       // (Optional) Next.js configuration
+└── styles               // (Optional) CSS files
