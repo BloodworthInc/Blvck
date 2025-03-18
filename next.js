908d8978d9c4ac53bf68361321851084c48bc68b
BloodworthInc <Mannybloodworth@icloud.com>
2025-03-18 07:26:42 -0500

    Update next.js

--- next.js
+++
@@ -1,8 +0,0 @@
-/your-project
-├── /pages
-│    ├── index.js         // Your homepage
-│    └── api              // (Optional) API endpoints
-├── /components          // (Optional) Reusable UI components
-├── package.json
-├── next.config.js       // (Optional) Next.js configuration
-└── styles               // (Optional) CSS files
